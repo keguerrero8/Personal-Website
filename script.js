8d781443d49009098b81ed7891b1483a8fc07918
Kevin Guerrero <guerrk83z@gmail.com>
2021-11-11 19:49:23 -0500

    added buttons and some content to professional page

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+/*Things to include in website:
+
+About me personally - husband, queens, power lifter, gamer, marvel and anime lover (maybe put video up of a lift?)
+Professional life - SBU BME, Siemens Engineer, now trying to be SW developer
+Resume
+Links (contact me) -  email and social media links - linkedin, twitter, GitHub
+Pictures?
+Projects? Can leave this blank for now
+*/
+const proPic1 = document.getElementById("button1");
+proPic1.addEventListener("click", function () { alert("This is timeline 1!")})
