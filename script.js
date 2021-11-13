1c7d3c4ecd8e13983b2c50b92eb57703e2a0d3cd
Kevin Guerrero <guerrk83z@gmail.com>
2021-11-12 20:26:10 -0500

    Completed professional page, cleaned up some items, updated background

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-/*Things to include in website:
-
-About me personally - husband, queens, power lifter, gamer, marvel and anime lover (maybe put video up of a lift?)
-Professional life - SBU BME, Siemens Engineer, now trying to be SW developer
-Resume
-Links (contact me) -  email and social media links - linkedin, twitter, GitHub
-Pictures?
-Projects? Can leave this blank for now
-*/
-
-
-const button = document.getElementById("button1");
-const popup = document.getElementById("popup")
-const closebutton = document.getElementById("closebutton");
+const button = document.getElementsByClassName("buttonBox");
+const popup = document.getElementsByClassName("popup")
+const closebutton = document.getElementsByClassName("closebutton");
 const overlay = document.getElementById("overlay");
 
-button.addEventListener("click", function () { 
-    overlay.style.display = "initial"
-    popup.style.display = "initial"
-})
+for (let i=0; i < button.length; i++) {
+    button[i].addEventListener("click", function () { 
+        overlay.style.display = "initial"
+        popup[i].style.display = "initial"
+    })
 
-closebutton.addEventListener("click", function () { 
-    overlay.style.display = "none"
-    popup.style.display = "none"})
+    closebutton[i].addEventListener("click", function () { 
+        overlay.style.display = "none"
+        popup[i].style.display = "none"
+    })
+}
+
+
 
 
